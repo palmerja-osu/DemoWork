6cb4ea7a34699a3324b994a91487ce2fbbe882bc
James I. Palmer <james.palmer@columbia.uniwest.com>
2017-11-03 09:58:25 -0700

    first demo works

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    public void PressedMenuButton()
+    {
+
+        Debug.Log("pressed menu button");
+    }
+
+    
+}
---
+++ Assets/Scripts/NullMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NullMenu : MonoBehaviour {
+
+    public void PressedNullButton()
+    {
+        Debug.Log("pressed null button");
+    }
+    public void PressedClearButton()
+    {
+        Debug.Log("pressed clear button");
+    }
+    public void PressedDisplayButton()
+    {
+        Debug.Log("pressed display button");
+    }
+}
+
