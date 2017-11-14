b35d65419da5a3754310859dde9bffc5c9db5c0c
James I. Palmer <james.palmer@columbia.uniwest.com>
2017-11-14 07:55:33 -0800

    added scrollsnap.content

---
+++ Assets/Scripts/PinchVersion2.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PinchVersion2 : MonoBehaviour
+{
+
+    public float perspectiveZoomSpeed = 0.5f;        // The rate of change of the field of view in perspective mode.
+    public float orthoZoomSpeed = 0.5f;        // The rate of change of the orthographic size in orthographic mode.
+    public Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+
+        if (Input.touchCount == 2)
+        {
+
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            //Find distances between each frame.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            // If the camera is orthographic...
+            if (cam.orthographic)
+            {
+                // ... change the orthographic size based on the change in distance between the touches.
+                cam.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
+
+                // Make sure the orthographic size never drops below zero.
+                cam.orthographicSize = Mathf.Max(cam.orthographicSize, 0.1f);
+            }
+            else
+            {
+                // Otherwise change the field of view based on the change in distance between the touches.
+                cam.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
+
+                // Clamp the field of view to make sure it's between 0 and 180.
+                cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, 0.1f, 10.9f);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/contentScrollRect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class contentScrollRect : MonoBehaviour
+{
+    public ScrollRect myScrollRect;
+    public RectTransform scrollableContent;
+
+    //Do this when the Save button is selected.
+    public void Start()
+    {
+        // assigns the contect that can be scrolled using the ScrollRect.
+        myScrollRect.content = scrollableContent;
+    }
+}
