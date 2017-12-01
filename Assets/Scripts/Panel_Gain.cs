9ee3ec2565961cb5254276dd44fb2f2037e16f9e
James I. Palmer <james.palmer@columbia.uniwest.com>
2017-12-01 10:04:27 -0800

    new gain changes

--- Assets/Scripts/Panel_Gain.cs
+++ Assets/Scripts/Panel_Gain.cs
@@ -1,22 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.EventSystems;
 
 public class Panel_Gain : MonoBehaviour
 {
-
+    [Header("Connecting Panels")]
     public GameObject ui;
     public GameObject Gain_MenuUI;
     public Options_Menu Options_Menu;
     public Small_PlaneToggle Small_PlaneToggle;
 
+    [Header("text")]
+    public Transform gainContentX;
+    public Text gainXOutput;
+
+    public Transform gainContentY;
+    public Text gainYOutput;
+
+    public double gainXNum = 0.0;
+    public double gainYNum = 0.0;
 
     public void Update()
     {
         //open inspector when menu button is pressed
         Gain_MenuUI.SetActive(true);
-
+        
     }
+    
 
     public void exitButton()
     {
@@ -24,6 +35,7 @@ public class Panel_Gain : MonoBehaviour
         returnToOptions();
         Debug.Log("pressed exit button");
     }
+    
     //exit button for second scene
     public void exitButton2()
     {
@@ -33,6 +45,94 @@ public class Panel_Gain : MonoBehaviour
         Debug.Log("pressed exit button");
     }
 
+    //***************************************
+    //      XGain functionality
+    //***************************************
+
+    //set the public variable from what keypadNum sets
+    public void UpdateXValue(double keypadNumX)
+    {
+        gainXNum = keypadNumX;
+        string gainxString = gainXNum.ToString();
+        gainXOutput.text = gainxString;
+    }
+
+    public void gainXButton()
+    {
+        Debug.Log("middleButton was pressed");
+        //press button send to gain keypad
+
+
+    }
+
+    public void plus6_XButton()
+    {
+        //add 6 to xNum
+        gainXNum = gainXNum + 6;
+    }
+    public void plus1_XButton()
+    {
+        //add 0.1 to xNum
+        gainXNum = gainXNum + 0.1;
+    }
+    public void minus1_XButton()
+    {
+        //minus 0.1 from xNum
+        gainXNum = gainXNum - 0.1;
+    }
+    public void minus6_XButton()
+    {
+        //minus 6 from xNum
+        gainXNum = gainXNum - 6;
+    }
+
+
+    //***************************************
+    //      YGain functionality
+    //***************************************
+
+    //set the public variable from what keypadNum sets
+    public void UpdateYValue(double keypadNumY)
+    {
+        gainYNum = keypadNumY;
+        string gainYString = gainYNum.ToString();
+        gainYOutput.text = gainYString;
+    }
+
+    public void gainYButton()
+    {
+        Debug.Log("middleButton was pressed");
+        //press button send to gain keypad
+
+
+    }
+
+    public void plus6_YButton()
+    {
+        //add 6 to xNum
+        gainYNum = gainYNum + 6;
+    }
+    public void plus1_YButton()
+    {
+        //add 0.1 to xNum
+        gainYNum = gainYNum + 0.1;
+    }
+    public void minus1_YButton()
+    {
+        //minus 0.1 from xNum
+        gainYNum = gainYNum - 0.1;
+    }
+    public void minus6_YButton()
+    {
+        //minus 6 from xNum
+        gainYNum = gainYNum - 6;
+    }
+
+
+
+    //***************************************
+    //      UI toggle options
+    //***************************************
     private void returnToOptions()
     {
 
